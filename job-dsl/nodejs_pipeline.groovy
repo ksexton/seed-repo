177aa709117f1a5573a277cdf001cfe8b06dcba3
Kyle Sexton <ks@mocker.org>
2017-09-20 13:25:14 -0500

    update

--- job-dsl/nodejs_pipeline.groovy
+++ job-dsl/nodejs_pipeline.groovy
@@ -1,10 +1,15 @@
-pipelineJob("nodejs-pipeline") {
-    scm {
-      git('git://github.com/ksexton/docker-nodejs-demo.git') {
+pipelineJob('nodejs-pipeline') {
+    definition {
+        cpsScm{
+          scm{
+               git('git://github.com/ksexton/docker-nodejs-demo.git') {
         node -> // is hudson.plugins.git.GitSCM
         node / gitConfigName('DSL User')
         node / gitConfigEmail('ks@mocker.org')
       }
-      scriptPath("misc/Jenkinsfile")
-    }  
+       
+          }
+          scriptPath('misc/Jenkinsfile')	
+        }
+    }
 }
