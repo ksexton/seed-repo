477e7cba3c44def92fa0d40a3b1ad53d21b9c568
Kyle Sexton <ks@mocker.org>
2017-10-17 13:45:35 -0500

    add git test

---
+++ job-dsl/gittag.groovy
@@ -0,0 +1,15 @@
+pipelineJob('gittag') {
+  definition {
+    cpsScm{
+      scm{
+        git('https://github.com/ksexton/docker-demo.git') {
+          node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('ks@mocker.org')
+        }
+
+      }
+      scriptPath('misc/Jenkinsfile-gittag')
+    }
+  }
+}
